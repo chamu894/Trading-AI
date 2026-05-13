c9fe35319ccbfde8a7c7a13cc72e5915a5da2e1b
chamudith bandara <chamudithbuddima@gmail.com>
2026-05-14 02:48:47 +0530

    update

--- strategy_engine/session_engine.py
+++ strategy_engine/session_engine.py
@@ -1,28 +1,28 @@
 from datetime import datetime
 import pytz
 
+
 def get_current_session():
 
-    # UTC time
     utc_now = datetime.now(pytz.utc)
 
     hour = utc_now.hour
 
-    # London Session
-    if 7 <= hour < 10:
+    # London Open
+    if 7 <= hour < 12:
         return "LONDON_OPEN"
 
-    # New York Session
-    elif 12 <= hour < 15:
-        return "NEWYORK_OPEN"
-
-    # London + NY overlap
+    # Overlap Session
     elif 12 <= hour < 16:
         return "OVERLAP"
 
+    # New York
+    elif 16 <= hour < 20:
+        return "NEWYORK_OPEN"
+
     else:
         return "OFF_SESSION"
-    
+
 
 def session_trade_allowed(session):
 
