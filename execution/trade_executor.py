734e1085101d221f72d9df91db5545de9d4ae57d
chamudith bandara <chamudithbuddima@gmail.com>
2026-05-14 02:47:27 +0530

    update

--- execution/trade_executor.py
+++ execution/trade_executor.py
@@ -2,9 +2,16 @@ import MetaTrader5 as mt5
 
 mt5.initialize()
 
+
 def place_order(symbol, order_type, lot, sl, tp):
 
-    price = mt5.symbol_info_tick(symbol).ask if order_type == "BUY" else mt5.symbol_info_tick(symbol).bid
+    tick = mt5.symbol_info_tick(symbol)
+
+    if tick is None:
+        print("❌ Symbol tick error")
+        return None
+
+    price = tick.ask if order_type == "BUY" else tick.bid
 
     request = {
         "action": mt5.TRADE_ACTION_DEAL,
@@ -14,7 +21,7 @@ def place_order(symbol, order_type, lot, sl, tp):
         "price": price,
         "sl": sl,
         "tp": tp,
-        "deviation": 10,
+        "deviation": 20,
         "magic": 10001,
         "comment": "AI TRADE",
         "type_time": mt5.ORDER_TIME_GTC,
@@ -22,5 +29,15 @@ def place_order(symbol, order_type, lot, sl, tp):
     }
 
     result = mt5.order_send(request)
+
+    if result is None:
+        print("❌ Order send failed")
+        return None
+
+    if result.retcode != mt5.TRADE_RETCODE_DONE:
+        print(f"❌ Trade failed: {result.retcode}")
+        return None
+
+    print("✅ Trade executed successfully")
 
     return result
